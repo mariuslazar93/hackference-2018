da8863e5cd561770f15dab78001e04486925df78
Marius Lazar <mariusl@packtpub.com>
2018-10-13 12:51:00 +0100

    initial commit with boilerplate files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+// Routes
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
